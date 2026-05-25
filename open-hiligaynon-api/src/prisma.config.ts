8c4561a4dda271a696f80a2e47d61737da91c3a3
LAPTOP-8JN3D935\ASUS <jdcamarin20@gmail.com>
2026-05-25 22:29:01 +0800

    ergretg

--- open-hiligaynon-api/src/prisma.config.ts
+++ open-hiligaynon-api/src/prisma.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("postgresql://hilitech_auth_db_user:3f3HxXJ3Xc3n4WuVOoQYbOgIqUpwVbDd@dpg-d84645jtqb8s73f2896g-a/hilitech_auth_db"),
+    url: "postgresql://hilitech_auth_db_user:3f3HxXJ3Xc3n4WuVOoQYbOgIqUpwVbDd@dpg-d84645jtqb8s73f2896g-a/hilitech_auth_db",
   },
 });
