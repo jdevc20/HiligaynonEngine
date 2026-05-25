0df71ce3942d801d04c686295a12899dcf2338dc
LAPTOP-8JN3D935\ASUS <jdcamarin20@gmail.com>
2026-05-25 21:51:13 +0800

    edfedf

--- open-hiligaynon-api/src/lib/prisma.ts
+++ open-hiligaynon-api/src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
-const connectionString = process.env.DATABASE_URL!;
+const connectionString = "postgresql://hilitech_auth_db_user:3f3HxXJ3Xc3n4WuVOoQYbOgIqUpwVbDd@dpg-d84645jtqb8s73f2896g-a/hilitech_auth_db";
 
 const pool = new pg.Pool({
   connectionString,
