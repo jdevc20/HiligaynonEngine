ac3bab5bcc1ae8faeceaaaf891e7866f60cb2802
LAPTOP-8JN3D935\ASUS <jdcamarin20@gmail.com>
2026-05-25 21:37:15 +0800

    [*]

--- open-hiligaynon-api/dist/lib/prisma.js
+++ open-hiligaynon-api/dist/lib/prisma.js
@@ -1,2 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+const connectionString = process.env.DATABASE_URL;
+const pool = new pg.Pool({
+    connectionString,
+});
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({
+    adapter,
+});
